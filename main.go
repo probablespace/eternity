b6cd3575baebe5b23fe6039ae7c976bb0f76ccd9
Marcus Bright <marcusbright@planitgeo.com>
2021-12-10 10:25:36 -0700

    fixes

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	nL "eternity/nymLib"
 
+	"fmt"
+	"io/ioutil"
+
 	"github.com/gorilla/websocket"
 )
 
@@ -16,25 +19,45 @@ func main() {
 
 	// could put some gateway bs here
 
+	// uri := "ws://localhost:1977"
+
+	// conn, _, err := websocket.DefaultDialer.Dial(uri, nil)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// wsh := nL.NewWebsocketHandler(conn)
+
+	// go wsh.RequestProcessor()
+	// println("starting reader routine")
+	// for {
+	// 	_, receivedResponse, err := wsh.Conn.ReadMessage()
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+
+	// 	request, err := nL.ParseReceived(receivedResponse)
+	// 	if err == nil {
+	// 		wsh.RequestQueue <- request
+	// 	}
+	// }
+	// check if we can marshal the reponse
 	uri := "ws://localhost:1977"
 
 	conn, _, err := websocket.DefaultDialer.Dial(uri, nil)
 	if err != nil {
 		panic(err)
 	}
-	wsh := nL.NewWebsocketHandler(conn)
+	defer conn.Close()
 
-	go wsh.RequestProcessor()
-	println("starting reader routine")
 	for {
-		_, receivedResponse, err := wsh.Conn.ReadMessage()
+		_, receivedResponse, err := conn.ReadMessage()
 		if err != nil {
 			panic(err)
 		}
 
-		request, err := nL.ParseReceived(receivedResponse)
-		if err == nil {
-			wsh.RequestQueue <- request
-		}
+		fileData, _ := nL.ParseReceived(receivedResponse)
+
+		fmt.Printf("writing the file back to the disk!\n")
+		ioutil.WriteFile("received_file_withreply", fileData.Body, 0644)
 	}
 }
